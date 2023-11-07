7b580eb072cebf80fc9e82d89b3600a12ecea5de
Tutku Ince <tutku.ince@outlook.com>
2023-11-07 21:38:31 +0300

    ProducerMessage model has been created

---
+++ spring-rabbit/src/main/java/io/ince/vms/springrabbit/model/ProducerMessage.java
@@ -0,0 +1,13 @@
+package io.ince.vms.springrabbit.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class ProducerMessage {
+    private Integer id;
+    private String message;
+}
