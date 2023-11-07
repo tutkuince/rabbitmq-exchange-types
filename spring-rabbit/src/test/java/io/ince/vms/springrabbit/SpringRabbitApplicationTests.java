e798371cfa4c18abdf33fecf472efb598b221da9
Tutku Ince <tutku.ince@outlook.com>
2023-11-07 20:34:40 +0300

    initial commit for spring-rabbit

---
+++ spring-rabbit/src/test/java/io/ince/vms/springrabbit/SpringRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.vms.springrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
