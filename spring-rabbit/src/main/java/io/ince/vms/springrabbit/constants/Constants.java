fd8e5aafe1cdadf1bce53ee17d99dd653acc6fd3
Tutku Ince <tutku.ince@outlook.com>
2023-11-07 21:38:09 +0300

    Constants class has been created

---
+++ spring-rabbit/src/main/java/io/ince/vms/springrabbit/constants/Constants.java
@@ -0,0 +1,11 @@
+package io.ince.vms.springrabbit.constants;
+
+public class Constants {
+
+    public static final String DIRECT_EXCHANGE = "direct.exchange";
+    public static final String QUEUE_A = "queue.A";
+    public static final String QUEUE_B = "queue.B";
+    public static final String ROUTING_A = "routing.A";
+    public static final String ROUTING_B = "routing.B";
+
+}
