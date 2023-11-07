9ae12eb2d74043ca24738787a4cf67dee05483ea
Tutku Ince <tutku.ince@outlook.com>
2023-11-07 22:19:47 +0300

    direct-consumer project has been created

---
+++ direct-consumer/src/test/java/io/ince/vms/directconsumer/DirectConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.vms.directconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
