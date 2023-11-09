25f91663778e782aa275fd0a09030cc2832be8b9
Tutku Ince <tutku.ince@outlook.com>
2023-11-09 20:58:28 +0300

    fanout-producer project initialization

---
+++ fanout-producer/src/main/java/io/ince/vms/fanoutproducer/FanoutProducerApplication.java
@@ -0,0 +1,13 @@
+package io.ince.vms.fanoutproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanoutProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanoutProducerApplication.class, args);
+    }
+
+}
---
+++ fanout-producer/src/test/java/io/ince/vms/fanoutproducer/FanoutProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.vms.fanoutproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanoutProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
