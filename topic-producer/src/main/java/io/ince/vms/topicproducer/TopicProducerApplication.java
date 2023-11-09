cc7e46eaa005a396c47fe1d2d7be5c2af767544a
Tutku Ince <tutku.ince@outlook.com>
2023-11-09 21:56:06 +0300

    topic-producer project initialzizer

---
+++ topic-producer/src/main/java/io/ince/vms/topicproducer/TopicProducerApplication.java
@@ -0,0 +1,13 @@
+package io.ince.vms.topicproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicProducerApplication.class, args);
+    }
+
+}
---
+++ topic-producer/src/test/java/io/ince/vms/topicproducer/TopicProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.vms.topicproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
