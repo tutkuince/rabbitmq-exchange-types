5880323e6437a3abe9f415386a5c622373ecc7d6
Tutku Ince <tutku.ince@outlook.com>
2023-11-09 22:15:18 +0300

    topic-consumer project init

---
+++ topic-consumer/src/test/java/io/ince/vms/topicconsumer/TopicConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.vms.topicconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
